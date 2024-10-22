1bc011c3382781144efd03cf6a45d3b39388f335
ajmaeen-98 <95845589+ajmaeenhelping@users.noreply.github.com>
2024-10-22 09:49:14 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<? include_once('common.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?=$site_name?></title>
+    <link href="images/logo.png" rel="icon" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="keyboard.js?ver=<?=$ver?>"></script>
+    <link rel="stylesheet" href="keyboard.css?ver=<?=$ver?>">
+    <link rel="stylesheet" href="<?= $site_url ?>assets/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css?ver=<?=$ver?>" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css?ver=<?=$ver?>" />
+</head>
